53e75025199b3c148232bb506e8b2a48bbc2c813
linbingjun <13425088053@139.com>
2018-11-14 14:06:32 +0800

    更新注释

--- demo/spring-boot-demo/src/main/java/cn/kknotes/open/demo/config/RequestBodyParamConfigurer.java
+++ demo/spring-boot-demo/src/main/java/cn/kknotes/open/demo/config/RequestBodyParamConfigurer.java
@@ -1,7 +1,11 @@
 package cn.kknotes.open.demo.config;
 
+import cn.kknotes.open.bean.MultiReadRequestBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import cn.kknotes.open.annotation.EnableRequestBodyParam;
+
+import javax.servlet.ServletRequest;
 
 /**
  * @author lin
@@ -11,22 +15,26 @@ import cn.kknotes.open.annotation.EnableRequestBodyParam;
 @EnableRequestBodyParam
 public class RequestBodyParamConfigurer {
 
-//    /**
-//     * 自定义 MultiReadHttpServletRequest 的使用条件
-//     * <p>没有定义 MultiReadRequestBean 时，会使用默认的 MultiReadRequestBean 条件
-//     * @see MvcMultiReadRequestConfigurer
-//     * @param request
-//     * @return
-//     */
-//    @Bean
-//    public MultiReadRequestBean testMultiReadRequestBean(ServletRequest request) {
-//        return new MultiReadRequestBean() {
-//            @Override
-//            public boolean filter(ServletRequest request) {
-//                //自定义条件
-//                return true;
-//            }
-//        };
-//    }
+    /**
+     * Customize MultiReadHttpServletRequest
+     * <p>Use default MultiReadRequestBean when don't has Customize MultiReadRequestBean
+     *
+     * 自定义 MultiReadHttpServletRequest 的使用条件
+     * <p>没有定义 MultiReadRequestBean 时，会使用默认的 MultiReadRequestBean 条件
+     *
+     * @see cn.kknotes.open.bean.DefaultMultiReadRequestBean
+     * @param request
+     * @return
+     */
+    @Bean
+    public MultiReadRequestBean testMultiReadRequestBean(ServletRequest request) {
+        return new MultiReadRequestBean() {
+            @Override
+            public boolean filter(ServletRequest request) {
+                //自定义条件
+                return true;
+            }
+        };
+    }
 
 }
